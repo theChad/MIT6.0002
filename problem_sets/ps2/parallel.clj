2c891021111ec2b466833da70d49b7e8aaf68857
Chad <theChad@users.noreply.github.com>
2021-03-26 15:22:44 -0500

    Parallel depth first search in clojure.
    Not much of a speedup, and actually gets really bogged down if I don't limit it
    to the first couple levels of recursion.

--- problem_sets/ps2/parallel.clj
+++ problem_sets/ps2/parallel.clj
@@ -1,4 +1,4 @@
-(ns ps2.core
+(ns ps2.parallel
   (:require [ps2.graph :as graph]))
 
 ;;;; Perform a depth-first search on a directed graph.
@@ -29,7 +29,7 @@
   (if (not path) nil
       [(conj (first path) node) (+ (second path) dist)]))
 
-(declare directed-dfs)
+(declare get-best-path)
 
 (defn next-dfs-call
   "Helper function to call directed-dfs with updated params.
@@ -39,16 +39,15 @@
   next-edge: edge to explore
   min-total-distance: min distance from dfs calls at this level"
   [digraph start end max-total-distance max-distance-outdoors
-   min-total-distance next-edge]
+   path best-dist min-total-distance next-edge]
   (let [new-max-total-dist (- (or min-total-distance max-total-distance)
                               (:total-distance next-edge))
         new-max-dist-outdoors (- max-distance-outdoors
                                  (:outdoor-distance next-edge))
-        new-start (:end-node next-edge)]
-    (update-path
-     (directed-dfs digraph new-start end new-max-total-dist
-                   new-max-dist-outdoors)
-     start (:total-distance next-edge))
+        new-start (:end-node next-edge)
+        new-path (update-path path (:end-node next-edge) (:total-distance next-edge))]
+    (get-best-path digraph new-start end new-max-total-dist
+                   new-max-dist-outdoors new-path best-dist)
     ))
 
 (defn roll-function
@@ -61,7 +60,17 @@
   shorter than previous explored paths on this branch. So
   return it, otherwise return the previous path prev-result."
   [f prev-result next-arg]
-  (or (f (second prev-result) next-arg) prev-result))
+  (let [new-path (f (second prev-result) next-arg)]
+    (if (every? nil? [new-path prev-result])
+      nil
+      (apply min-key second (filter some? [new-path prev-result])))))
+
+(defn choose-shortest-path
+  "Choose the shortest from among a collection of paths.
+  Nil if no paths"
+  [paths]
+  (if (every? nil? paths) nil
+      (apply min-key second (filter some? paths))))
 
 ;;; directed-dfs will call itself recursively, with the help
 ;;; of next-dfs-call to set up parameters and complete the
@@ -72,22 +81,28 @@
 ;;; a path that satisfies the max constraints (which includes
 ;;; being no longer than a path already found).
 
-(defn directed-dfs
+(defn get-best-path
   "Perform a depth-first search on a directed graph.
   digraph: A directed graph, type graph/Digraph
   start: string representing starting node
   end: string representing ending node
   max-total-dist: maximum distance on path
-  max-dist-outdoors: maximum outdoor distance"
-  [digraph start end max-total-distance max-dist-outdoors]
+  max-dist-outdoors: maximum outdoor distance
+  path: [[nodes visited] total-dist outdoor-dist],
+        path visited so far."
+
+  [digraph start end max-total-distance max-dist-outdoors path best-dist]
+  ;;(if (< 3 (count (first path))) (println "thread" (.getName (Thread/currentThread))))
   (cond
     (not (and (graph/contains-node? digraph start)
-              (graph/contains-node? digraph end)))
-    ;; Nodes not in digraph, return nil
+              (graph/contains-node? digraph end)
+              (< (second path) @best-dist)))
+    ;; Nodes not in digraph, or path is longer than shortest path, return nil
     nil
     (= start end)
-    ;; At the end, return the path.
-    [(list end) 0]
+    ;; At the end, update best-dist and return the path.
+    (do (swap! best-dist #(min % (second path)))
+        path)
     :else
     (let [next-edges (valid-edges digraph max-total-distance max-dist-outdoors
                                   (graph/get-edges-from-source digraph start))
@@ -97,12 +112,24 @@
       ;; an updated max-total-distance. That also guarantees that
       ;; new results from directed-dfs will be shorter paths than
       ;; old results (if it doesn't return nil), so or will take care of it.
-      (reduce (partial roll-function
-                       (partial next-dfs-call digraph start end
-                                max-total-distance max-dist-outdoors))
-              nil next-edges)
+
+      (let [map-type (if (< (count (first path)) 3) pmap map)]
+        (choose-shortest-path
+         (doall (map-type (partial next-dfs-call digraph start end
+                           max-total-distance max-dist-outdoors
+                           path best-dist nil)
+                  next-edges)))) 
       )))
 
+
+(defn directed-dfs
+  "Perform the depth-first search.
+  This function is here for parameter initialization."
+  [digraph start end max-total-distance max-dist-outdoors]
+  (let [best-path 
+        (get-best-path digraph start end max-total-distance max-dist-outdoors [[start] 0] (atom ##Inf))]
+    (shutdown-agents)
+    best-path))
 
 (defn test-dfs
   [opts]
@@ -110,6 +137,5 @@
         start "1"
         end "32"
         max-total-distance 999
-        max-dist-outdoors 0]
-    (println (directed-dfs digraph start end max-total-distance max-dist-outdoors))
-    (println ((:nodes digraph) "end"))))
+        max-dist-outdoors 90]
+    (println (time (directed-dfs digraph start end max-total-distance max-dist-outdoors)))))
